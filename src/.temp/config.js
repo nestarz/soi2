ce8bf8119981c8b4b20143ef9e651c5046752ddb
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-05-30 11:40:54 +0200

    remove temp

--- src/.temp/config.js
+++
@@ -1,7 +0,0 @@
-export default {
-  "siteUrl": "https://eliasrhouzlane.com",
-  "siteName": "Gridsome",
-  "titleTemplate": "%s | Elias Rhouzlane",
-  "siteDescription": "",
-  "version": "0.5.7"
-}
