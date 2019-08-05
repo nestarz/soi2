3ece2ae911621642795783d4fcdbaf6e09b9985d
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-08-06 00:40:59 +0200

    update

--- src/functions/sharp.js
+++ src/functions/sharp.js
@@ -4,13 +4,18 @@ const glob = require("globby");
 const sharp = require("sharp");
 
 (async () => {
-  (await glob("static/instagram/images/*.png")).map(image =>
+  (await glob("static/instagram/original/*.png")).map(image =>
     sharp(image)
       .resize(500)
-      .webp({ quality: 90 })
+      .webp({
+        lossless: false,
+        quality: 90,
+        force: true,
+        reductionEffort: 6
+      })
       .toFile(
         path.join(
-          path.dirname(image),
+          "static/instagram/images/", //path.dirname(image),
           path.format({
             root: "/",
             name: path.basename(image, ".png"),
