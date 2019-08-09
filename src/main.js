f51546498504ecc5faa79ca5c54d1e2d2c68921a
Elias Rhouzlane <elias.rhouzlane@gmail.com>
2019-08-09 20:53:14 +0200

    update withotu vuex

--- src/main.js
+++ src/main.js
@@ -1,37 +1,32 @@
-import 'circular-std';
+import "circular-std";
 // This is the main.js file. Import global CSS and scripts here.
-import '~/assets/style/index.scss';
+import "~/assets/style/index.scss";
 
 // Import default layout so we don't need to import it to every page
-import VueMasonry from 'vue-masonry-css';
+import VueMasonry from "vue-masonry-css";
 
-
-import Vuex from 'vuex';
-import VueCytoscape from 'vue-cytoscape';
-import DefaultLayout from '~/layouts/default.vue';
-import App from '~/App';
+import VueCytoscape from "vue-cytoscape";
+import DefaultLayout from "~/layouts/default.vue";
+import App from "~/App";
 
 const langs = {
   fr: {
-    slug: 'fr',
-    name: 'Français',
+    slug: "fr",
+    name: "Français"
   },
   en: {
-    slug: 'en',
-    name: 'English',
-  },
+    slug: "en",
+    name: "English"
+  }
 };
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-export default function (Vue, {
-  router, head, isClient, appOptions,
-}) {
-  Vue.use(Vuex);
+export default function(Vue, { router, head, isClient, appOptions }) {
   Vue.use(VueMasonry);
   Vue.use(VueCytoscape);
 
   // Set default layout as a global component
-  Vue.component('layout', DefaultLayout);
+  Vue.component("layout", DefaultLayout);
 
   appOptions.render = h => h(App);
 }
